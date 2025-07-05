2a3b0df3b582810a26045604ab61d47fe5b83954
arif1018 <51040618+arif1018@users.noreply.github.com>
2025-07-05 23:56:40 +0500

    Add product controller and routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getUsers } = require('../controllers/userController');
+const productRoutes = require('./products');
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.use('/products', productRoutes);
 
 module.exports = router;
